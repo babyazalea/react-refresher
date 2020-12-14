a2acc27d868b83bd415fdb06cb9e40410d402f73
babyazalea <usun12@hotmail.com>
2020-12-15 02:01:08 +0900

    using component & rendering list of data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
 
+import GoalList from "./components/GoalList";
+import "./App.css";
+
 const App = () => {
-  return <h1 title="this works">A React App!</h1>; // React.createElement('h1', {title: ''this works}, "A React App!")
+  const courseGoals = [
+    { id: "cg1", text: "Finish the Course" },
+    { id: "cg2", text: "Learn all about the Course Main Topic" },
+    { id: "cg3", text: "Help other students in the Course Q&A" },
+  ];
+
+  return (
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <GoalList goals={courseGoals} />
+    </div>
+  );
 };
 
 export default App;
